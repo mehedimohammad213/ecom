48d788b481f445147a5f30a033a3a4b780a028be
MEHEDI HASAN <110008381+mehedimohammad213@users.noreply.github.com>
2023-04-07 02:24:22 +0600

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
-import {dirname} from 'dirname';
+
 import {fileURLToPath} from 'url';
 import cors from "cors";
 
